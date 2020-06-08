ce1b29e7919463cb467f08f4054d5cdc03d7f4fe
Andrea <andrea@andreabarr.dev>
2020-06-08 17:11:06 -0500

    added DisplayEstablishments.js; removed header in App.js

--- est-backend/src/App.js
+++ est-backend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import DisplayEstablishments from './components/DisplayEstablishments'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+        <DisplayEstablishments />
     </div>
   );
 }
