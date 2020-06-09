8e1d5296722814ad07932f297cf12aef3af8c4e2
Andrea <andrea@andreabarr.dev>
2020-06-08 19:28:05 -0500

    created getEstablishments fn

--- est-backend/src/components/DisplayEstablishments.js
+++ est-backend/src/components/DisplayEstablishments.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
+import { listEstablishments } from '../graphql/queries'
+import { API, graphqlOperation } from 'aws-amplify'
 
 class DisplayEstablishments extends Component {
+
+	componentDidMount = async () => {
+		this.getEstablishments()
+	}
+
+	getEstablishments = async () => {
+		const result = await API.graphql(graphqlOperation(listEstablishments))
+		console.log("All Establishments...", JSON.stringify(result.data.listEstablishments.items))
+	}
 
 	render() {
 		return(
